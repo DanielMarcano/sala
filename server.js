d269a1ba953433986fdb4dc842dbb55b2df6f956
DanielMarcano <danielemarcano96@gmail.com>
2018-07-08 21:23:07 +0200

    Add gzip compression to the project

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/dist')));
