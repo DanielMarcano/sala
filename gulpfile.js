fae71fa1cc76c03cfe3d2097eb069d3f2458fbb4
DanielMarcano <danielemarcano96@gmail.com>
2018-08-14 02:14:00 +0200

    Project has been restructured
    SASS has been integrated
    404 page added
    Images have been optimized

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,29 @@ const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync');
 const inlinesource = require('gulp-inline-source');
 const babel = require('gulp-babel');
-const smushit = require('gulp-smushit');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const inject = require('gulp-inject');
+const imagemin = require('gulp-imagemin');
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+const imageminPngQuant  = require ('imagemin-pngquant');
 
-const smush = () => {
-  return gulp.src('./public/img/*')
-    .pipe(smushit({
-      verbose: true,
-    }))
-    .pipe(gulp.dest('./dist/img'));
-};
+gulp.task('optimize', () => {
+  return gulp.src('public/img/*')
+    .pipe(imagemin([
+      imagemin.gifsicle(),
+      imageminJpegRecompress({
+        loops: 6,
+        min: 40,
+        max: 85,
+        quality: 'low',
+      }),
+      imageminPngQuant(),
+      imagemin.svgo(),
+    ]))
+
+    .pipe(gulp.dest('dist/img'));
+});
 
 // const images = () => {
 //   return gulp.src('./public/img/*')
@@ -28,10 +42,13 @@ const fonts = () => {
 };
 
 const inline = () => {
-  return gulp.src('./public/index.html')
+  return gulp.src('./public/***/*.html')
     .pipe(inlinesource())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 };
+
+gulp.task('inline', inline);
 
 const startBrowserSync = (done) => {
   browserSync.init({
@@ -40,17 +57,32 @@ const startBrowserSync = (done) => {
   done();
 };
 
-const clean = () => {
+const compileSass = () => {
+  return gulp.src('./public/sass/pages/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
+};
+
+gulp.task('compile:sass', compileSass);
+
+const optimizeCSS = () => {
   return gulp.src('./public/css/*.css')
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(cleanCSS({
+      compatibility: 'ie8',
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream());
 };
 
-const defaultContent = (done) => {
-  console.log('This is my default content');
-  done();
-};
+// const defaultContent = (done) => {
+//   console.log('This is my default content');
+//   done();
+// };
 
 const translateAndUglify = () => {
   return gulp.src('./public/js/*.js')
@@ -62,31 +94,50 @@ const translateAndUglify = () => {
 
 gulp.task('uglify-js', translateAndUglify);
 
-const defaultTasks = gulp.series(gulp.parallel('uglify-js'), defaultContent);
+// const defaultTasks = gulp.series(gulp.parallel('uglify-js'), defaultContent);
 
-module.exports = { default: defaultTasks };
-
-gulp.task('scripts', (done) => {
-  console.log('Running the scripts gulp task');
-  done();
-});
-
-gulp.task('styles', (done) => {
-  console.log('Running the styles gulp task');
-  done();
-});
-
-gulp.task('default', gulp.series(translateAndUglify, inline, smush, fonts, startBrowserSync));
+// module.exports = { default: defaultTasks };
 
 const watcherReporter = (path, stats) => {
   console.log(`File ${path} was changed`);
 };
 
-gulp.task('watch', gulp.series('default', () => {
-  const jsWatcher = gulp.watch('./public/js/**/*.js', gulp.series(translateAndUglify, inline));
-
+const watch = () => {
+  const jsWatcher = gulp.watch('./public/**/*.js', gulp.series(translateAndUglify, inline));
   jsWatcher.on('change', watcherReporter);
 
-  const cssWatcher = gulp.watch('./public/css/**/*.css', gulp.series(clean, inline));
-  cssWatcher.on('change', watcherReporter);
-}));
+  // const cssWatcher = gulp.watch('./public/**/*.css', gulp.series(optimizeCSS, inline));
+  // cssWatcher.on('change', watcherReporter);
+
+  const scssWatcher = gulp.watch('./public/**/*.scss', gulp.series(compileSass, optimizeCSS, inline));
+  scssWatcher.on('change', watcherReporter);
+};
+
+gulp.task('watch', watch);
+
+const injectIndex = (done) => {
+  const target = gulp.src('./public/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  const sources = gulp.src(['./public/**/*.css'], {
+    read: false,
+  });
+
+  // gulp.src('./public/index.html')
+  //   .pipe(inject(gulp.src(['./public/**/*.js'], ['./public/**/*.css']), {
+  //     starttag: '<!-- inject:{{path}} -->',
+  //     relative: true,
+  //     transform: (filePath, file) => {
+  //       // return file contents as string
+  //       return file.contents.toString('utf8');
+  //     },
+  //   }))
+  //   .pipe(gulp.dest('./dist'));
+  // done();
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./dist'));
+};
+
+gulp.task('injectIndex', injectIndex);
+
+gulp.task('default', gulp.series(compileSass, optimizeCSS, translateAndUglify, inline, fonts, startBrowserSync, watch));
--- server.js
+++ server.js
@@ -6,11 +6,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.disable('x-powered-by');
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/dist'));
+app.use(express.urlencoded({
+  extended: true,
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/dist')));
+
+app.use((req, res, next) => {
+  return res.status(404).sendFile('/dist/html/error.html', {
+    root: __dirname,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and running at ${PORT}`);
