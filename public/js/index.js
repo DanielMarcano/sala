58a46b24dbcbbefc95186e31658db433b097d56f
DanielMarcano <danielemarcano96@gmail.com>
2018-10-12 17:37:16 +0200

    W.I.P. contact page is being made

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,8 @@ window.onload = function () {
   const body = document.getElementsByTagName('body')[0];
   window.scrollTo({ left: 0, top: body.scrollHeight });
 
-  const words = ["artistas", "actorxs", "soñadorxs", "cantantes", "bailarinxs", "humanxs", "rarxs", "grandes", "pequeñxs", "amantes", "hippies", "cineastas", "pintorxs"];
+  const words = ['artistas', 'actorxs', 'soñadorxs', 'cantantes', 'bailarinxs', 'humanxs', 'rarxs', 'grandes', 'pequeñxs', 'amantes', 'hippies', 'cineastas', 'pintorxs', 'productores', 'directores', 'dramaturgos', 'escritores', 'guionistas', 'cantantes', 'músicos'];
+  
   const people = document.getElementsByClassName('js-main__people')[0];
   people.innerHTML = "";
 
