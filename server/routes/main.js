d7c0f149c051e8515668a1dce1c3c8e1894138c7
DanielMarcano <danielemarcano96@gmail.com>
2018-11-02 12:54:07 +0100

    Improved overall styles of the site

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 router.get('/', (req, res) => {
   res.status(200).render('index');
@@ -17,44 +17,51 @@ router.get('/programacion', (req, res) => {
   res.status(200).render('html/programacion');
 });
 
-// router.get('/alquiler', (req, res) => {
-//   res.status(200).render('html/alquiler');
-// });
+router.get('/alquiler', (req, res) => {
+  res.status(200).render('html/alquiler');
+});
 
-// const sendMail = function (req, res) {
-//   console.log('puta');
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     auth: {
-//       user: 'rprd5e5ai4hw3etw@ethereal.email',
-//       pass: 'CuBMcSNfhQhEdSjVCq',
-//     },
-//   });
+const sendMail = function (req, res) {
 
-//   const mailOptions = {
-//     from: '"Sala On" <rprd5e5ai4hw3etw@ethereal.email>',
-//     to: 'danielemarcano96@gmail.com',
-//     subject: 'Hello ',
-//     text: 'Hello world?',
-//     html: '<b>Hello world?</b>',
-//   };
+  const output = `
+    <p>Nombre: ${req.body.name}</p>
+    <p>Apellido: ${req.body.lastname}</p>
+    <p>Email: ${req.body.email}</p>
+    <p>Teléfono: ${req.body.phone}</p>
+    <p>Empresa: ${req.body.business}</p>
+    <p>Más sobre ti: ${req.body.more}</p>
+  `;
 
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     console.log('Message sent: %s', info.messageId);
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'salaonbcnalquileres@gmail.com',
+      pass: '123456Portland',
+    },
+  });
 
-//     res.render('html/alquiler', { success: 'El mensaje se ha enviado correctamente' });
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//   });
-// };
+  const mailOptions = {
+    from: '"Sala On" <salaonbcnalquileres@gmail.com>',
+    to: 'danielemarcano96@gmail.com',
+    subject: 'Nueva Petición de Alquiler',
+    html: output,
+  };
 
-// router.post('/pedir_alquiler', sendMail);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.render('html/alquiler', { success: 'El mensaje se ha enviado correctamente' });
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
+};
+
+router.post('/pedir_alquiler', sendMail);
 
 router.use((req, res, next) => {
   return res.status(404).render('html/error');
