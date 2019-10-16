fb1be2f0fe26940a6ca8b23e12bbf0fbbf198057
DanielMarcano <danielmarcanodev@gmail.com>
2019-10-17 01:42:32 +0200

    Add environment variablesss

---
+++ server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // 1. MongoDB
+    MONGO_URI: process.NODE_ENV != 'production' ? 'mongodb://127.0.0.1:27017/salaonbcn' : 'mongodb+srv://roberton:Xtqw6BQm29RziXbq@cluster0-wgnaf.mongodb.net/test?retryWrites=true&w=majority',
+  
+    // 2. JWT
+    // TOKEN_SECRET: process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L',
+  
+    // 3. Express Server Port
+    PORT: process.env.PORT || 3000,
+    secret: 'worldisfullofdevelopers'
+  };
