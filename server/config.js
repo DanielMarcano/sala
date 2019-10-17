8b2401b9ed193913db2457ea6acb8045916d42e7
DanielMarcano <danielmarcanodev@gmail.com>
2019-10-17 02:03:55 +0200

    Fix it

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 1. MongoDB
-    MONGO_URI: process.NODE_ENV != 'production' ? 'mongodb://127.0.0.1:27017/salaonbcn' : 'mongodb+srv://roberton:Xtqw6BQm29RziXbq@cluster0-wgnaf.mongodb.net/test?retryWrites=true&w=majority',
+    MONGO_URI: process.env.NODE_ENV != 'production' ? 'mongodb://127.0.0.1:27017/salaonbcn' : 'mongodb+srv://roberton:Xtqw6BQm29RziXbq@cluster0-wgnaf.mongodb.net/test?retryWrites=true&w=majority',
   
     // 2. JWT
     // TOKEN_SECRET: process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L',
