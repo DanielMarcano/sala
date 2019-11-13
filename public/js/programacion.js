ffc893311eb0f0d418ff9febc831c12f5b90bf8c
DanielMarcano <danielmarcanodev@gmail.com>
2019-11-14 00:52:27 +0100

    Fixed navigation issue and also the JS of the programacion screen...

--- public/js/programacion.js
+++ public/js/programacion.js
@@ -1,30 +1,32 @@
-const sinopsisButtons = document.querySelectorAll('.event__sinopsis');
-const closeButtons = document.querySelectorAll('.popup__close');
-const headerHeight = document.querySelector('header').scrollHeight;
+window.addEventListener('DOMContentLoaded', () => {
+  const sinopsisButtons = document.querySelectorAll('.event__sinopsis');
+  const closeButtons = document.querySelectorAll('.popup__close');
+  const headerHeight = document.querySelector('.header').scrollHeight;
 
-const addClass = element => document.documentElement.classList.add('u-disable-scroll');
-const removeClass = element => document.documentElement.classList.remove('u-disable-scroll');
-const scrollToId = (selector) => {
-  document.querySelector(`a[href='#${selector}']`).scrollIntoView();
-  document.documentElement.scrollTop -= headerHeight;
-};
+  const disableScroll = () => document.documentElement.classList.add('u-disable-scroll');
+  const enableScroll = () => document.documentElement.classList.remove('u-disable-scroll');
+  const sinopsisClickHandler = (event) => {
+    disableScroll();
+  }
 
-const removeScroll = () => {
-  sinopsisButtons.forEach((element) => {
-    element.addEventListener('click', addClass);
-  });
-};
+  const initializeSinopsisButtons = () => {
+    sinopsisButtons.forEach((element) => element.addEventListener('click', sinopsisClickHandler));
+  };
 
-const addScroll = () => {
-  closeButtons.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      removeClass();
-      setTimeout(() => {
-        scrollToId(event.target.getAttribute('data-back'));
-      }, 100);
-    });
-  });
-};
+  const scrollToId = (selector) => {
+    document.querySelector(`a[href='#${selector}']`).scrollIntoView();
+    document.documentElement.scrollTop -= headerHeight;
+  };
 
-removeScroll();
-addScroll();
+  const closeButtonClickHandler = (event) => {
+    enableScroll();
+    setTimeout(() => scrollToId(event.target.getAttribute('data-back')), 0);
+  }
+
+  const initializeCloseButtons = () => {
+    closeButtons.forEach((element) => element.addEventListener('click', closeButtonClickHandler));
+  };
+
+  initializeSinopsisButtons();
+  initializeCloseButtons();
+});
